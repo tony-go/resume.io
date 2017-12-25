dc1b67f2c6c6e6e1e008397d403952052e85650e
Tony Gorez <gorez.tony@gmail.com>
2017-12-25 20:26:58 +0100

    Education Page Update

--- src/Component/Education/Education.js
+++ src/Component/Education/Education.js
@@ -1,24 +1,44 @@
 import React from 'react';
-import { Collapsible, CollapsibleItem } from 'react-materialize'
 
 import './Education.css';
 
 class Education extends React.Component{
+
+
+
 	render(){
 		return(
-			<div className="Education" >
-				<Collapsible accordion>
-					<CollapsibleItem header='First' icon='filter_drama'>
-						Lorem ipsum dolor sit amet.
-					</CollapsibleItem>
-					<CollapsibleItem header='Second' icon='place'>
-						Lorem ipsum dolor sit amet.
-					</CollapsibleItem>
-					<CollapsibleItem header='Third' icon='whatshot'>
-						Lorem ipsum dolor sit amet.
-					</CollapsibleItem>
-				</Collapsible>
-			</div>
+		<div className="Education">
+			<ul className="timeline">
+				<li>
+					<div className="direction-r">
+						<div className="flag-wrapper">
+							<span className="flag">Freelancer</span>
+							<span className="time-wrapper"><span className="time">2013 - present</span></span>
+						</div>
+						<div className="desc">My current employment. Way better than the position before!</div>
+					</div>
+				</li>
+				<li>
+					<div className="direction-l">
+						<div className="flag-wrapper">
+							<span className="flag">Apple Inc.</span>
+							<span className="time-wrapper"><span className="time">2011 - 2013</span></span>
+						</div>
+						<div className="desc">My first employer. All the stuff I've learned and projects I've been working on.</div>
+					</div>
+				</li>
+				<li>
+					<div className="direction-r">
+						<div className="flag-wrapper">
+							<span className="flag">Harvard University</span>
+							<span className="time-wrapper"><span className="time">2008 - 2011</span></span>
+						</div>
+						<div className="desc">A description of all the lectures and courses I have taken and my final degree?</div>
+					</div>
+				</li>
+			</ul>
+		</div>
 		)
 	}
 }
