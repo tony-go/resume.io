3d35666a47a030dc6c0f9955b055f9b802e0a77d
Tony Gorez <gorez.tony@gmail.com>
2017-12-25 20:28:00 +0100

    Work Page update

--- src/Component/Work/Work.js
+++ src/Component/Work/Work.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import $ from 'jquery'; 
 
-import { Col, Card } from 'react-materialize'
+import { Row, Col, Card, Chip } from 'react-materialize'
 
 import './Work.css';
 
@@ -10,22 +10,61 @@ class Work extends React.Component{
 	render(){
 
 		return(
-			<div className="Work" >
-				<Col m={12} s={12}>
-					<Card className='grey lighten-4' textClassName='white-text' title='Card title' actions={[<a href='#'>This is a link</a>]}>
-					I am a very simple card.
-					</Card>
-				</Col>
-				<Col m={12} s={12}>
-					<Card className='grey lighten-4' textClassName='white-text' title='Card title' actions={[<a href='#'>This is a link</a>]}>
-					I am a very simple card.
-					</Card>
-				</Col>
-				<Col m={12} s={12}>
-					<Card className='grey lighten-4' textClassName='white-text' title='Card title' actions={[<a href='#'>This is a link</a>]}>
-					I am a very simple card.
-					</Card>
-				</Col>
+			<div className="Work wrap" >
+				<h1 className="section-title wow fadeInUp" >Experiences</h1>
+				<Row>
+					<Col l={10} m={12} s={12}>
+						<Card 
+							className='grey lighten-4 card' 
+							textClassName='black-text' 
+							title='Customer Happiness' 
+							actions={[
+							<Chip>ReactJs</Chip>,
+							<Chip>Adobe Suite</Chip>,
+							<Chip>Trello</Chip>
+							]}
+							>
+							<em>GetQuanty</em>
+							<p>I am the bridge between the customers and the dev team.</p>
+						</Card>
+					</Col>
+				</Row>
+				<Row>
+					<Col l={10} m={12} s={12}>
+						<Card 
+							className='grey lighten-4 card' 
+							textClassName='black-text' 
+							title='Project Manager' 
+							actions={[
+							<Chip>SEO</Chip>,
+							<Chip>Social Media</Chip>,
+							<Chip>Growth Hacking</Chip>,
+							<Chip>HTML</Chip>
+							]}
+							>
+							<em>Fullsix</em>
+							<p>Managing of different projects in the famous parisian digital agency.</p>
+						</Card>
+					</Col>
+				</Row>
+				<Row>	
+					<Col  l={10} m={12} s={12}>
+						<Card 
+							className='grey lighten-4 card' 
+							textClassName='black-text' 
+							title='Web Content Integrator / CM' 
+							actions={[
+							<Chip>Wordpress</Chip>,
+							<Chip>Adobe Suite</Chip>,
+							<Chip>Community Management</Chip>,
+							<Chip>HTML / CSS</Chip>
+							]}
+							>
+							<em>Freelance</em>
+							<p>Managing of different projects in the famous parisian digital agency.</p>
+						</Card>
+					</Col>
+				</Row>
 			</div>
 		)
 	}
