5c5d41c75e399d98a95927b5b56c360312b352e1
Tony <gorez.tony@gmail.com>
2017-12-25 16:37:58 +0100

    Work page update

--- src/Component/Work/Work.js
+++ src/Component/Work/Work.js
@@ -1,23 +1,31 @@
 import React from 'react';
-import { Collapsible, CollapsibleItem } from 'react-materialize'
+import $ from 'jquery'; 
+
+import { Col, Card } from 'react-materialize'
 
 import './Work.css';
 
 class Work extends React.Component{
+
 	render(){
+
 		return(
 			<div className="Work" >
-				<Collapsible accordion>
-					<CollapsibleItem header='First' icon='filter_drama'>
-						Lorem ipsum dolor sit amet.
-					</CollapsibleItem>
-					<CollapsibleItem header='Second' icon='place'>
-						Lorem ipsum dolor sit amet.
-					</CollapsibleItem>
-					<CollapsibleItem header='Third' icon='whatshot'>
-						Lorem ipsum dolor sit amet.
-					</CollapsibleItem>
-				</Collapsible>
+				<Col m={12} s={12}>
+					<Card className='grey lighten-4' textClassName='white-text' title='Card title' actions={[<a href='#'>This is a link</a>]}>
+					I am a very simple card.
+					</Card>
+				</Col>
+				<Col m={12} s={12}>
+					<Card className='grey lighten-4' textClassName='white-text' title='Card title' actions={[<a href='#'>This is a link</a>]}>
+					I am a very simple card.
+					</Card>
+				</Col>
+				<Col m={12} s={12}>
+					<Card className='grey lighten-4' textClassName='white-text' title='Card title' actions={[<a href='#'>This is a link</a>]}>
+					I am a very simple card.
+					</Card>
+				</Col>
 			</div>
 		)
 	}
