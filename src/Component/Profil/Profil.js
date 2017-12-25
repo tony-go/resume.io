234d64aede98e381ee6c49ace9c2c8ef8bf66aa1
Tony <gorez.tony@gmail.com>
2017-12-25 16:36:10 +0100

    Profil Adjustment

--- src/Component/Profil/Profil.js
+++ src/Component/Profil/Profil.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './Profil.css'
 import Tony from './tony.jpg'
 
+import { Link } from 'react-router-dom';
+
 class Profil extends React.Component{
 	render(){
 		return (
 			<div className="Profil col 12" >
 				<div className="Profil-image row 12">
-					<img src={Tony} alt="tony gorez" className="circle responsive-img z-depth-4" />
+					<Link to="/">
+						<img src={Tony} alt="tony gorez" className="circle responsive-img z-depth-4" />
+					</Link>
 				</div>
 				<div className="Profil-text left-align row 12">
 					<h1>Tony Gorez</h1>
