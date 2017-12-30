62bcc7dbc82c4b867b2086217e1387683a82d651
Tony Gorez <gorez.tony@gmail.com>
2017-12-30 22:47:58 +0100

    Social media bar

--- src/Component/Hobbies/Hobbies.js
+++ src/Component/Hobbies/Hobbies.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import bodymovin from 'bodymovin'
 import animationData from './hobbies.json'
+import { Row } from 'react-materialize'
 
-
+import './Hobbies.css'
 
 class Hobbies extends React.Component {
 
@@ -16,7 +17,19 @@ class Hobbies extends React.Component {
         return (
         <div className="wrap">
             <h1>Hobbies</h1>
-            <div ref={(animationDiv) => { this.animationContainer = animationDiv; }}/>
+            <Row>
+                <div ref={(animationDiv) => { this.animationContainer = animationDiv; }}/>
+            </Row>
+            <Row>
+                <div className="social-media">
+                    <a href="#"><span>Facebook</span></a>
+                    <a href="#"><span>Twitter</span></a>
+                    <a href="#"><span>Google+</span></a>
+                    <a href="#"><span>Github</span></a>
+                    <a href="#"><span>Dribbble</span></a>
+                    <a href="#"><span>CodePen</span></a>
+                </div>
+            </Row>
         </div>
         )
     }
