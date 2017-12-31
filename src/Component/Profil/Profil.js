000b42faf6bd06cfa699812d37db917978e7c85b
Tony Gorez <gorez.tony@gmail.com>
2017-12-31 20:56:00 +0100

    Profil responsive

--- src/Component/Profil/Profil.js
+++ src/Component/Profil/Profil.js
@@ -7,15 +7,25 @@ import { Link } from 'react-router-dom';
 class Profil extends React.Component{
 	render(){
 		return (
-			<div className="Profil col 12" >
-				<div className="Profil-image row 12">
+			<div className="Profil col 12 s12" >
+				<div className="Profil-image">
 					<Link to="/">
 						<img src={Tony} alt="tony gorez" className="circle responsive-img z-depth-4" />
 					</Link>
 				</div>
-				<div className="Profil-text left-align row 12">
+				<div className="Profil-text col 12 s12">
 					<h1>Tony Gorez</h1>
 					<h2>Front-end <br/> Developer</h2>
+					<span>
+						<a href="tel:+33 6 28 74 18 36">
+							<i class="fa fa-phone  fa-2x" aria-hidden="true"></i>
+						</a>
+					</span>
+					<span>
+						<a mailto="gorez.tony@gmail.com">
+							<i class="fa fa-envelope  fa-2x" aria-hidden="true"></i>
+						</a>
+					</span>
 				</div>
 			</div>
 		)
