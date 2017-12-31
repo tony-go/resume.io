85444f6bd065acd4b277d0b699656894fe1e19de
Tony Gorez <gorez.tony@gmail.com>
2017-12-31 20:55:30 +0100

    Menu component Update

--- src/Component/Menu/Menu.js
+++ src/Component/Menu/Menu.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './Menu.css'
+import { Col } from 'react-materialize'
 
 import { Link } from 'react-router-dom';
 
 class Menu extends React.Component{
 	render(){
 		return (
-			<div className="Menu col 12" >
+			<Col s={12}>
 				<div className="Menu-link" >
 					<ul className="pagination">
-						<li className="waves-effect"><a href="#!"><i className="material-icons">chevron_left</i></a></li>
+						<li className="waves-effect"><i className="material-icons">remove</i></li>
 						<li className="waves-effect menu-element">
 							<Link to='/skills'>
 								<i className="material-icons">equalizer</i>
@@ -25,10 +26,10 @@ class Menu extends React.Component{
 								<i className="material-icons">library_books</i>
 							</Link>
 						</li>
-						<li className="waves-effect"><a href="#!"><i className="material-icons">chevron_right</i></a></li>
+						<li className="waves-effect"><i className="material-icons">remove</i></li>
 					</ul>
 				</div>
-			</div>
+			</Col>
 		)
 	}
 }
